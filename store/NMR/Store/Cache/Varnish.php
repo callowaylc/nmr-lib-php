b734cc28c6cb04a0e768057cc7b0265321c40bc4
Christian <christian.calloway@nomorerack.com>
2013-10-30 00:16:24 -0400

    added body definition to method Varnish#purge

--- store/NMR/Store/Cache/Varnish.php
+++ store/NMR/Store/Cache/Varnish.php
@@ -1,5 +1,6 @@
 <?php /** @author Christian Calloway callowaylc@gmail christian@nomorerack */
 namespace NMR\Store\Cache;
+use Guzzle\Http\Client;
 
 /** Honestly not a big believer in interfaces, but since we want
  ** to draw a common interface between all cache handling instances
@@ -8,7 +9,23 @@ namespace NMR\Store\Cache;
 class Varnish extends Cache {
 
 	/** Alias to Cache#delete; allowing for static calls as well */
-	public function purge($key) { }
+	public function purge($key) {
+
+		// create a guzzle client user current server
+		// @TODO we need to abstract this into a varnish client, since
+		// hosting server will NOT be localhost and $_SERVER can be 
+		// spoofed anyways
+		// @TODO need to do error checking against key
+		// @TODO add asynch to request
+		$client = new Client(
+			"http://{$_SERVER['SERVER_ADDR']}:{$_SERVER['SERVER_PORT']}/"
+		);
+		$response = $client->createRequest('PURGE', $key)->send();
+
+		// check response and manage exception in the event that
+		// request could not be fulfilled
+		var_export($response); exit;
+	}
 
 	public function write($key, $ttl = null, $value) {
 		// @PASS
